e28a529cf515d6888dad3fbb4122cf711191aecb
alexfisher03 <fisherr.alexander@gmail.com>
2025-02-25 19:33:13 -0500

    Added error handling

---
+++ QV-app/src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let error;
+	export let status;
+</script>
+
+<svelte:head>
+	<title>{status} - Something went wrong</title>
+</svelte:head>
+
+<section>
+    <h1 class="text-xl font-bold text-white">{status}</h1>
+    <p class="body-text">{error.message}</p>
+    <p class="text-sm">Sorry, something went wrong on our end. Please try again later.</p>
+</section>
+
+<style>
+    section {
+        background-image: url(../src/lib/images/body-image-error.png);
+        min-height: 100vh;
+	    margin: 0;
+	    background-attachment: fixed;
+	    background-size: 100vw 100vh;
+	    background-color: #1e1e1e;
+    }
+</style>
